1cdc04538e968c671e081023a0a0a47124717037
K <k@MacBook-Air-de-DUNA.local>
2026-09-17 09:48:41 -0600

    2026-09-17 DUUN: Eventos 17-19 septiembre (FERCO+ZONDA, NAT SALAZAR+TATA GAYA, ARRAZATE+1 TAPS), limpieza de assets viejos

--- src/app/pages/shows/events.ts
+++ src/app/pages/shows/events.ts
@@ -25,19 +25,42 @@ export interface ShowEvent {
 
 export const EVENTS: ShowEvent[] = [
   {
-    id: 260910,
-    day: '10',
+    id: 260917,
+    day: '17',
     month: 'SEPTIEMBRE',
-    title: 'DANNA GODEL + JULIA TOPOREK',
+    title: 'FERCO + ZONDA',
     venue: 'DUNA CLUB',
     city: 'Querétaro',
-    dateShort: '10/09',
+    dateShort: '17/09',
     time: '21:00',
-    description: 'DANNA GODEL + JULIA TOPOREK en DUNA CLUB. Una noche de música electrónica.',
-    photos: [
-      'assets/jueves-10-septiembre/danna-godel.jpg',
-      'assets/jueves-10-septiembre/julia-toporek.jpg'
-    ],
-    ticketsUrl: 'https://shows.link/duna-club-julia-toporek-danna-godel/tickets'
+    description: 'FERCO + ZONDA en DUNA CLUB. Una noche de música electrónica.',
+    photos: ['assets/17-19septiembre/flyer-17-septiembre.jpg'],
+    ticketsUrl: 'https://shows.link/s/dn-clb-frc'
+  },
+  {
+    id: 260918,
+    day: '18',
+    month: 'SEPTIEMBRE',
+    title: 'NAT SALAZAR + TATA GAYA',
+    venue: 'DUNA CLUB',
+    city: 'Querétaro',
+    dateShort: '18/09',
+    time: '21:00',
+    description: 'NAT SALAZAR + TATA GAYA en DUNA CLUB. Una noche de música electrónica.',
+    photos: ['assets/17-19septiembre/flyer-18-septiembre.jpg'],
+    ticketsUrl: 'https://shows.link/s/dn-clb-nat'
+  },
+  {
+    id: 260919,
+    day: '19',
+    month: 'SEPTIEMBRE',
+    title: 'ARRAZATE + 1 TAPS',
+    venue: 'DUNA CLUB',
+    city: 'Querétaro',
+    dateShort: '19/09',
+    time: '21:00',
+    description: 'ARRAZATE + 1 TAPS en DUNA CLUB. Una noche de música electrónica.',
+    photos: ['assets/17-19septiembre/flyer-19-septiembre.jpg'],
+    ticketsUrl: 'https://shows.link/s/dn-clb-arrzt'
   }
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import '@google/model-viewer';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
